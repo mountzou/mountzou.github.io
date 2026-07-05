ccd2ccd58bbb04c509df98ac5d860deba02b0a90
Christos Mountzouris <chrismountzou@gmail.com>
2026-07-05 23:37:41 +0300

    minor improvements

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,12 @@
 export const SITE_URL = 'https://mountzou.github.io';
 
+export const socialLinks = {
+  linkedin: 'https://www.linkedin.com/in/mountzou',
+  github: 'https://github.com/mountzou',
+  researchgate: 'https://www.researchgate.net/profile/Christos-Mountzouris',
+  googleScholar: 'https://scholar.google.com/citations?user=xenZggUAAAAJ&hl=en',
+} as const;
+
 export const siteMeta = {
   defaultTitle: 'Chris Mountzouris | CV & Portfolio',
   defaultDescription: 'CV & Portfolio of Chris Mountzouris.',
